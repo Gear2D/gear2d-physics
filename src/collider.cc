1575d5e8c46c8137830079215786e7d404747b47
Leonardo Guilherme de Freitas <leonardo.guilherme@gmail.com>
2013-02-17 15:44:48 -0300

    Vector prototype

---
+++ src/collider.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "gear2d.h"
+
+#include "vector3.h"
+
+using namespace gear2d;
+using namespace std;
+
+class collider : public component::base {
+  private:
+    
+    vector3 v;
+    
+  public:
+    // constructor and destructor
+    collider() { }
+    virtual ~collider() { }
+    
+    void writepos(const vector3 & v) {
+      write<float>("x", v.x);
+      write<float>("y", v.y);
+      write<float>("z", v.z);
+    }
+    
+    void readpos(vector3 & v) {
+      read<float>("x", v.x);
+      read<float>("y", v.y);
+      read<float>("z", v.z);
+    }
+    
+    
+    // greeter family
+    virtual gear2d::component::family family() { return "collider"; }
+    
+    // helloperson type
+    virtual gear2d::component::type type() { return "collider"; }
+    
+    virtual std::string depends() { return "spatial"; }
+    
+    // setup phase, to initialize paramters and other stuff
+    virtual void setup(object::signature & sig) {
+      v.x = v.y = v.z = 0;
+      write("speed", v);
+      
+      float h = v.x + v.y;
+    }           
+    
+    
+    virtual void update(timediff dt) {
+    }
+};
+
+// the build function
+extern "C" { component::base * build() { return new collider(); } }
---
+++ src/vector3.h
@@ -0,0 +1,54 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+#include "gear2d.h"
+#include <cmath>
+
+using namespace gear2d;
+using std::sqrt;
+
+
+class vector3 {
+  private:
+    float x;
+    float y;
+    float z;
+    float lenght;
+    
+  public:
+    vector3(float x = 0, float y = 0, float x = 0)
+    : x(x), y(y), z(z)
+    {
+      
+      float a = 1.0f + lenght;
+    }
+    
+    void write(component::base & com, const std::string & prefix) {
+      com.write<float>(prefix + ".x", x);
+      com.write<float>(prefix + ".y", y);
+      com.write<float>(prefix + ".z", z);
+    }
+    
+    void read(component::base & com, const std::string & prefix) {
+      com.read<float>(prefix + ".x", x);
+      com.read<float>(prefix + ".y", y);
+      com.read<float>(prefix + ".z", z);
+    }
+    
+    
+    /* static methods */
+    static vector3 from(component::base & com, const std::string & prefix) {
+      vector3 v;
+      v.read(com, prefix);
+      return v;
+    }
+    
+    void bolas() {
+      
+    }
+    
+};
+
+
+
+#endif
